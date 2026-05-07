906244df438c2425a78255790c62baafb2857c49
`Kamogelo Tshukudu <kamogelotshukudu@AOLs-MacBook-Pro.local>
2026-05-07 09:48:46 +0200

    chore: bump versionCode to 4 for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         applicationId = "com.example.jbmarks"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
